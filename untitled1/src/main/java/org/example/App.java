5b9a53572eb43887879856e81ae7539cf01f59ba
stu <stu@123.com>
2022-07-22 17:06:50 +0800

    1

---
+++ untitled1/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
